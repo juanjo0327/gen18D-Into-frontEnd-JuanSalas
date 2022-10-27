564e6bda774509236644cb81284dba5707af94c4
juansalasz <juanjosalas100@gmail.com>
2022-10-26 19:36:36 -0600

    feat: Se suben los cambios con los componentes en el App.js

--- trainers-pokemon/src/App.js
+++ trainers-pokemon/src/App.js
@@ -1,12 +1,18 @@
+import React from 'react';
+import Dashboard from './components/Dashboard';
 import NavBar from "./components/NavBar";
+import SearchFilters from './components/SearchFilters';
 function App() {
   return (
-    <>
+    <React.Fragment>
       <div>
         <span>componente App</span>
       </div>
       <NavBar></NavBar>
-    </>
+      <SearchFilters></SearchFilters>
+      <Dashboard></Dashboard>
+
+    </React.Fragment>
     );
 }
 
